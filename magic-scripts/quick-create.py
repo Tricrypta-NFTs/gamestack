887574e0e1a1f2991cfe5bc8c427d31671b09c1c
Nicholas Deckard <nicholas.deckard@crazymagic.studio>
2022-08-26 15:03:04 +0000

    add unreal engine configurations

--- magic-scripts/quick-create.py
+++ magic-scripts/quick-create.py
@@ -1,4 +1,8 @@
 import os
 
-#os.system("sudo bash ./p4d-files/install-p4d.sh")
-#os.system("bash ./init-cdk.sh")
+os.system("sudo bash ./p4d-files/install-p4d.sh")
+os.system("bash ./init-cdk.sh")
+load_balancer = input("GameStudioStack.LoadBalancer: ")
+os.system("bash ./p4d-files/configure-p4v.sh super " + load_balancer)
+project_name = input("Project Name: ")
+os.system("bash ./p4d-files/create-stream.sh " + project_name)
